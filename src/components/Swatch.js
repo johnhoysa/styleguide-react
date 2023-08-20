6b976325a2f7e5e2df95ecce2bc2c1068c42166d
johnhoysa <johnhoysa@gmail.com>
2023-08-20 18:18:01 -0500

    color and swatch testing

--- src/components/Swatch.js
+++ src/components/Swatch.js
@@ -1,18 +1,12 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import styled from "styled-components";
 
-const SwatchTitleParent = styled.h3`
-  color: red;
-`;
-
-const Swatch = ({ swatchColor, swatchTitle, swatchDescription }) => {
+const Swatch = ({ color, title, description }) => {
   return (
     <div className="swatch">
-      <p className="swatch-color">color: {swatchColor}</p>
-      <p>{swatchTitle}</p>
-      <SwatchTitleParent>{swatchTitle}</SwatchTitleParent>
-      <p>{swatchDescription}</p>
+      <p className="">color: {color}</p>
+      <p>{title}</p>
+      <p>{description}</p>
     </div>
   );
 };
