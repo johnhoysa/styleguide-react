da70e3beb9271f01e0032ad055328be980f73eac
johnhoysa <johnhoysa@gmail.com>
2023-08-21 16:36:48 -0500

    Colors pulling in global variable

--- src/styleguide/Colors.js
+++ src/styleguide/Colors.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Swatch from '../components/Swatch.js';
-
+import TitleBar from '../components/TitleBar.js';
 const Colors = () => {
   return (
     <section>
-      <div className="page-title">Colors</div>
+      <TitleBar title="Colors" />
 
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-3 gap-3 mt-6">
         <span className="">Preview</span>
         <span className="">CSS</span>
         <span className="">Value</span>
       </div>
-      <hr class="bg-gray-800"></hr>
+
+      <hr className="bg-gray-800"></hr>
       <Swatch color="bg-sky" name="sky" value="#9C8EF1" />
       <hr></hr>
       <Swatch color="bg-success" name="success" value="HEX #green" />
