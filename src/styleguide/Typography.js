6838f9a023d432445f162396c7a44e639c813589
johnhoysa <johnhoysa@gmail.com>
2023-08-21 19:01:20 -0500

    in process of creating typography page

---
+++ src/styleguide/Typography.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TitleBar from '../components/TitleBar.js';
+
+const Colors = () => {
+  return (
+    <section>
+      <TitleBar title="Typography" />
+    </section>
+  );
+};
+
+export default Colors;
