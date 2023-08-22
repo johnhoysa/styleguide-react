57409f4d7b9e857b9354c46948f8d8e6d35ef23a
johnhoysa <johnhoysa@gmail.com>
2023-08-22 10:58:48 -0500

    Text Component, Typography and CSS updates

--- src/styleguide/Typography.js
+++ src/styleguide/Typography.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TitleBar from '../components/TitleBar.js';
 import Heading from '../components/Heading.js';
+import Text from '../components/Text.js';
 
 const Typography = () => {
   return (
@@ -30,6 +31,26 @@ const Typography = () => {
         fontFamily="font-serif"
         type="display-three"
       />
+
+      <h5 className="mt-4 guide-heading">Body Text</h5>
+      <Text Tag="p" text="Body Large: DM Sans Regular, 20px" type="body-lg" />
+      <Text
+        Tag="p"
+        text="Body Primary: DM Sans Regular, 16px"
+        type="body-primary"
+      />
+      <Text Tag="p" text="Body Small: DM Sans Regular, 14px" type="body-sm" />
+
+      <h5 className="mt-4 guide-heading">Button Text</h5>
+      <Text Tag="p" text="Button Xl: DM Sans Bold, 20px" type="button-xl" />
+      <Text
+        Tag="p"
+        text="Button Primary: DM Sans Regular, 18px"
+        type="button-primary"
+      />
+      <Text Tag="p" text="Button Md: DM Sans Bold, 18px" type="button-md" />
+      <Text Tag="p" text="Button Sm: DM Sans Bold, 16px" type="button-sm" />
+      <Text Tag="p" text="Button Xs: DM Sans Bold, 14px" type="button-xs" />
     </div>
   );
 };
