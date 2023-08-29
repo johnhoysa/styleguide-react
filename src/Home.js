d4b5877a6826e574df968f241762f060b5b97013
johnhoysa <johnhoysa@gmail.com>
2023-08-29 11:44:03 -0500

    homepage content updated

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import TitleBar from './components/TitleBar.js';
+
 const Home = () => {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <section>
+        <TitleBar title="Welcome" />
+        <h5 className="guide-heading">Example</h5>
+      </section>
+      <section className="guide-section">
+        <h1>Page Content Needed</h1>
+        <ul className="pl-6 list-disc">
+          <li>React</li>
+          <li>Tailwind</li>
+        </ul>
+      </section>
+    </>
+  );
 };
 
 export default Home;
